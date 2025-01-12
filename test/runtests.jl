3bfbfe6742e7c8ba49634854021e0f5c9e76cdff
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-01-12 11:44:30 -0600

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,22 @@ using Aqua
         Aqua.test_all(Chairmarks, deps_compat=false, persistent_tasks=false)
         Aqua.test_deps_compat(Chairmarks, check_extras=false)
     end
+
+    @testset "@benchmarkable" begin
+        benchmark = @benchmarkable 100 rand evals=7 samples=1729
+        @test ChairmarksForAirspeedVelocity.BenchmarkTools.tune!(benchmark) === nothing
+        result = run(benchmark)
+        @test result.params.evals == 7
+        @test result.params.samples == 1729
+        @test result.times isa Vector{Float64}
+        @test result.gctimes isa Vector{Float64}
+        @test 10 < minimum(result.times) < 100_000 # measured in nanoseconds
+        @test maximum(result.gctimes) < 100_000_000 # measured in nanoseconds (could be zero)
+        @test result.memory isa Int
+        @test result.allocs isa Int
+    end
+
+    @testset "BenchmarkGroup" begin
+        BenchmarkGroup()
+    end
 end
